b245c227201913e99bdb564a847a30691c9cc1f5
Liukin <contacto@pc-solucion.es>
2025-04-02 11:25:04 +0200

    Añadida funcionalidad de mostrar los users que ascienden un puesto

--- script.js
+++ script.js
@@ -18,101 +18,122 @@ const leaderboard = [
   {
     name: 'JAMES_193',
     team: 'mercedes',
-    gap: '+542'
+    gap: '+542',
+    posicion: 1
   },
   {
     name: 'TAKERU_XIII',
     team: 'mercedes',
-    gap: '+456'
+    gap: '+456',
+    posicion: 1
   },
   {
     name: 'DARKOUS666',
     team: 'ferrari',
-    gap: '+392'
+    gap: '+392',
+    posicion: 1
   },
   {
     name: 'X1LENZ',
     team: 'red bull',
-    gap: '+387'
+    gap: '+387',
+    posicion: 1
   },
   {
     name: 'BROXA24',
     team: 'ferrari',
-    gap: '+373'
+    gap: '+373',
+    posicion: 1
   },
   {
     name: 'MANGUERAZO',
     team: 'red bull',
-    gap: '+332'
+    gap: '+332',
+    posicion: 1
   },
   {
     name: 'RACTOR09',
     team: 'alpine',
-    gap: '+317'
-  }, {
+    gap: '+317',
+    posicion: 2
+  }, 
+  {
     name: 'macusam',
     team: 'astonmartin',
-    gap: '+316'
+    gap: '+316',
+    posicion: 1
   },
   {
     name: 'CCXSNOP',
     team: 'alfaRomeo',
-    gap: '+295'
+    gap: '+295',
+    posicion: 1
   },
   {
     name: 'XROOCKK',
     team: 'toroRosso',
-    gap: '+279'
+    gap: '+279',
+    posicion: 1
   },
   {
     name: 'Romain Grosjean',
     team: 'haas',
-    gap: '+1 lap'
+    gap: '+1 lap',
+    posicion: 1
   },
   {
     name: 'Lance Stroll',
     team: 'racing point',
-    gap: '+1 lap'
+    gap: '+1 lap',
+    posicion: 1
   },
   {
     name: 'Kevin Magnussen',
     team: 'haas',
-    gap: '+1 lap'
+    gap: '+1 lap',
+    posicion: 1
   },
   {
     name: 'Carlos Sainz',
     team: 'mclaren',
-    gap: '+1 lap'
+    gap: '+1 lap',
+    posicion: 1
   },
   {
     name: 'Antonio Giovinazzi',
     team: 'alfa romeo',
-    gap: '+1 lap'
+    gap: '+1 lap',
+    posicion: 1
   },
   {
     name: 'George Russell',
     team: 'williams',
-    gap: '+2 laps'
+    gap: '+2 laps',
+    posicion: 1
   },
   {
     name: 'Robert Kubica',
     team: 'williams',
-    gap: '+2 laps'
+    gap: '+2 laps',
+    posicion: 1
   },
   {
     name: 'Lando Norris',
     team: 'mclaren',
-    gap: 'DNF'
+    gap: 'DNF',
+    posicion: 1
   },
   {
     name: 'Daniil Kvyat',
     team: 'toro rosso',
-    gap: 'DNF'
+    gap: 'DNF',
+    posicion: 1
   },
   {
     name: 'Nico Hulkenberg',
     team: 'renault',
-    gap: 'DNF'
+    gap: 'DNF',
+    posicion: 1
   }
 ];
 
@@ -136,8 +157,13 @@ const drivers = main
 // posición usando el índice de los puntos de datos
 drivers
   .append('td')
-  .text((d, i) => i + 1)
-  .attr('class', 'position');
+  .attr('class', 'position')
+  .html((d, i) => {
+    const positionImage = d.posicion === 2 
+      ? '<img class="updown" src="https://res.cloudinary.com/pcsolucion/image/upload/v1743585029/Position_Up_gbv0y4.png" alt="Position Up" style="height: 1em; vertical-align: middle; display: block; margin-bottom: 2px;">'
+      : '';
+    return `${positionImage}${i + 1}`;
+  });
 
 // nombre seguido del equipo
 drivers
@@ -151,13 +177,13 @@ drivers
       : team === 'red bull' 
       ? '<img src="https://res.cloudinary.com/pcsolucion/image/upload/v1742786100/rb_x5pwuu.png" alt="Red Bull" style="height: 1em; vertical-align: middle;">' 
       : team === 'alpine' 
-      ? '<img src="https://res.cloudinary.com/pcsolucion/image/upload/v1742786254/imgbin_alpine-a110-sports-car-renault-png_upq6ni.png" alt="Red Bull" style="height: 1em; vertical-align: middle;">'
+      ? '<img src="https://res.cloudinary.com/pcsolucion/image/upload/v1742786254/imgbin_alpine-a110-sports-car-renault-png_upq6ni.png" alt="Alpine" style="height: 1em; vertical-align: middle;">'
       : team === 'astonmartin' 
-      ? '<img src="https://res.cloudinary.com/pcsolucion/image/upload/v1742786664/am_wuvn6v.png" alt="Red Bull" style="height: 1em; vertical-align: middle;">'
+      ? '<img src="https://res.cloudinary.com/pcsolucion/image/upload/v1742786664/am_wuvn6v.png" alt="Aston Martin" style="height: 1em; vertical-align: middle;">'
       : team === 'alfaRomeo' 
-      ? '<img src="https://res.cloudinary.com/pcsolucion/image/upload/v1742786780/20210106191506_Alfa_Romeo_Racing__Logo_fly4r8.svg" alt="Red Bull" style="height: 1em; vertical-align: middle;">'
+      ? '<img src="https://res.cloudinary.com/pcsolucion/image/upload/v1742786780/20210106191506_Alfa_Romeo_Racing__Logo_fly4r8.svg" alt="Alfa Romeo" style="height: 1em; vertical-align: middle;">'
       : team === 'toroRosso' 
-      ? '<img src="https://res.cloudinary.com/pcsolucion/image/upload/v1742786903/racing_bulls-logo_brandlogos.net_bjuef_ygq2em.png" alt="Red Bull" style="height: 1em; vertical-align: middle;">'
+      ? '<img src="https://res.cloudinary.com/pcsolucion/image/upload/v1742786903/racing_bulls-logo_brandlogos.net_bjuef_ygq2em.png" alt="Toro Rosso" style="height: 1em; vertical-align: middle;">'
       : team;
     
     return `${name.split(' ').map((part, index) => index > 0 ? `<strong>${part}</strong>` : `${part}`).join(' ')} <span>${teamName}</span>`;
